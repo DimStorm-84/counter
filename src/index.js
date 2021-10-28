91a27f6e65380e283897dcb1cab7a8024985a2fc
Dima <motevich.dmitrij@gmail.com>
2021-10-28 11:09:12 +0300

    new 28.10

--- src/index.js
+++ src/index.js
@@ -11,20 +11,20 @@ ReactDOM.render(
   //<React.StrictMode>
   <App />,
   //</React.StrictMode>,
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import "bootstrap/dist/css/bootstrap.css";
+ // import Counter from "./components/counter";
 
 ReactDOM.render(
   <React.StrictMode>
     <Counter />
   </React.StrictMode>,
->>>>>>> ca2987e8900dd5a4519f2c72ab2c974f9bc7edad
+// >>>>>>> ca2987e8900dd5a4519f2c72ab2c974f9bc7edad
   document.getElementById("root")
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals()
